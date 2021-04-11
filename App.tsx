5d9b30b47f779737ba635928e9857becff0f5f9b
Zhang Yao <typescript59@gmail.com>
2021-04-11 23:40:50 +0200

    fix logic issue

--- App.tsx
+++ App.tsx
@@ -8,8 +8,8 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
-import Geolocation, { GeoPosition } from 'react-native-geolocation-service';
+import React, {useState} from 'react';
+import Geolocation, {GeoPosition} from 'react-native-geolocation-service';
 import {
   Alert,
   Linking,
@@ -26,108 +26,110 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const hasLocationPermissionsIOS = async (): Promise<boolean> => {
-  const openSettings = () => {
-    Linking.openSettings().catch(() => {
-      Alert.alert('Unable to open settings');
-    });
-  };
-  const status = await Geolocation.requestAuthorization('whenInUse');
-  if (status === 'granted') {
-    return true;
-  }
-  if (status === 'denied') {
-    Alert.alert('Location Permission was denied');
-  }
-  if (status === 'disabled') {
-    Alert.alert(
-      'Turn on Location Services to allow this app to determine your location.',
-      '',
-      [
-        {text: 'Go to Settings', onPress: openSettings},
-        {text: "Don't Use Location", onPress: () => {}},
-      ],
-    );
-  }
-  return false;
-};
 
-const hasLocationPermissions = async () => {
-  if (Platform.OS === 'ios') {
-    const hasPermission = await hasLocationPermissionsIOS();
-    return hasPermission;
-  }
 
-  // We assume that location permission is already granted by user under 23 level for android
-  if (Platform.OS === 'android' && Platform.Version < 23) {
-    return true;
-  }
-
-  const hasPermission = await PermissionsAndroid.check(
-    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-  );
-
-  if (hasPermission) {
-    return true;
-  }
-
-  const status = await PermissionsAndroid.request(
-    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-  );
-
-  if (status === PermissionsAndroid.RESULTS.GRANTED) {
-    return true;
-  }
-
-  if (status === PermissionsAndroid.RESULTS.DENIED) {
-    ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
-  } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
-    ToastAndroid.show(
-      'Location permission revoked by user.',
-      ToastAndroid.LONG,
-    );
-  }
-
-  return false;
-};
-
-const getLocation = async (
-  setLocation: (location: GeoPosition) => void,
-) => {
-  const hasPermission = await hasLocationPermissions();
-  if (!hasPermission) {
-    return;
-  }
-
-  Geolocation.getCurrentPosition(
-    (position) => {
-      setLocation(position);
-    },
-    (error) => {
-      Alert.alert(`Code ${error.code}`, error.message);
-      console.log(error);
-    }
-  );
-
-}
-
-const App = async () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const [location, setLocation] = useState({});
+
+  const hasLocationPermissionsIOS = async (): Promise<boolean> => {
+    const openSettings = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('whenInUse');
+    if (status === 'granted') {
+      return true;
+    }
+    if (status === 'denied') {
+      Alert.alert('Location Permission was denied');
+    }
+    if (status === 'disabled') {
+      Alert.alert(
+        'Turn on Location Services to allow this app to determine your location.',
+        '',
+        [
+          {text: 'Go to Settings', onPress: openSettings},
+          {text: "Don't Use Location", onPress: () => {}},
+        ],
+      );
+    }
+    return false;
+  };
+  
+  const hasLocationPermissions = async () => {
+    if (Platform.OS === 'ios') {
+      const hasPermission = await hasLocationPermissionsIOS();
+      return hasPermission;
+    }
+  
+    // We assume that location permission is already granted by user under 23 level for android
+    if (Platform.OS === 'android' && Platform.Version < 23) {
+      return true;
+    }
+  
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+  
+    if (hasPermission) {
+      return true;
+    }
+  
+    const status = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+  
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    }
+  
+    if (status === PermissionsAndroid.RESULTS.DENIED) {
+      ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
+    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      ToastAndroid.show(
+        'Location permission revoked by user.',
+        ToastAndroid.LONG,
+      );
+    }
+  
+    return false;
+  };
+  
+  const getLocation = async () => {
+    const hasPermission = await hasLocationPermissions();
+    if (!hasPermission) {
+      return;
+    }
+  
+    Geolocation.getCurrentPosition(
+      position => {
+        setLocation(position);
+      },
+      error => {
+        Alert.alert(`Code ${error.code}`, error.message);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+      },
+    );
+  };
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  await getLocation(setLocation);
+  getLocation();
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -135,14 +137,15 @@ const App = async () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <View>
+        <View style={styles.container}>
           <Text>Your location is:</Text>
-            <Text>Latitude: {location?.coords?.latitude || ''}</Text>
-            <Text>Longitude: {location?.coords?.latitude || ''}</Text>
-          <Pressable onPress={async () => {
-            await getLocation(setLocation);
-          }}>
-            <Text>Get Location</Text>;
+          <Text>Latitude: {location?.coords?.latitude || ''}</Text>
+          <Text>Longitude: {location?.coords?.latitude || ''}</Text>
+          <Pressable
+            onPress={async () => {
+              await getLocation();
+            }}>
+            <Text>Get Location</Text>
           </Pressable>
         </View>
       </ScrollView>
@@ -151,22 +154,11 @@ const App = async () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  }
 });
 
 export default App;
