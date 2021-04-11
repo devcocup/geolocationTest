30966c03b756f105fc966ec30f32471e3be21bd9
Zhang Yao <typescript59@gmail.com>
2021-04-11 21:28:22 +0200

    reolve lint issue

--- App.tsx
+++ App.tsx
@@ -8,35 +8,35 @@
  * @format
  */
 
- import React, { useEffect } from 'react';
- import Geolocation from 'react-native-geolocation-service';
- import {
-   Alert,
-   Linking,
-   Platform,
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+import React, {useEffect} from 'react';
+import Geolocation from 'react-native-geolocation-service';
+import {
+  Alert,
+  Linking,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
- const hasLocationPermissionsIOS = async (): Promise<boolean> => {
-   const openSettings = () => {
-     Linking.openSettings().catch(() => {
+const hasLocationPermissionsIOS = async (): Promise<boolean> => {
+  const openSettings = () => {
+    Linking.openSettings().catch(() => {
       Alert.alert('Unable to open settings');
-     });
-   }
+    });
+  };
   const status = await Geolocation.requestAuthorization('whenInUse');
   if (status === 'granted') {
     return true;
@@ -46,123 +46,122 @@
   }
   if (status === 'disabled') {
     Alert.alert(
-      `Turn on Location Services to allow this app to determine your location.`,
+      'Turn on Location Services to allow this app to determine your location.',
       '',
       [
-        { text: 'Go to Settings', onPress: openSettings },
-        { text: "Don't Use Location", onPress: () => {} },
+        {text: 'Go to Settings', onPress: openSettings},
+        {text: "Don't Use Location", onPress: () => {}},
       ],
     );
   }
   return false;
- }
+};
 
- const hasLocationPermissions = async () => {
-   if (Platform.OS === 'ios') {
-     const hasPermissions = await hasLocationPermissionsIOS();
-     return hasPermissions;
-   }
- }
+const hasLocationPermissions = async () => {
+  if (Platform.OS === 'ios') {
+    const hasPermissions = await hasLocationPermissionsIOS();
+    return hasPermissions;
+  }
+};
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   useEffect(async () => {
-     console.log("shit");
-    const granted = await Geolocation.requestAuthorization("always");
+const Section: React.FC<{
+  title: string;
+}> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(async () => {
+    console.log('shit');
+    const granted = await Geolocation.requestAuthorization('always');
     if (granted) {
       Geolocation.getCurrentPosition(
-        (position) => {
+        position => {
           console.log(position);
         },
-        (error) => {
+        error => {
           // See error code charts below.
           console.log(error.code, error.message);
-        }
-    );
+        },
+      );
     }
-    
-   });
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+  });
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
- export default App;
+export default App;
