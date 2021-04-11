aaab54e51236eeef924f3a53fef71da6577f2ac7
Zhang Yao <typescript59@gmail.com>
2021-04-11 21:32:34 +0200

    complete the location permission check function

--- App.tsx
+++ App.tsx
@@ -13,12 +13,14 @@ import Geolocation from 'react-native-geolocation-service';
 import {
   Alert,
   Linking,
+  PermissionsAndroid,
   Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  ToastAndroid,
   useColorScheme,
   View,
 } from 'react-native';
@@ -62,6 +64,38 @@ const hasLocationPermissions = async () => {
     const hasPermissions = await hasLocationPermissionsIOS();
     return hasPermissions;
   }
+
+  // We assume that location permission is already granted by user under 23 level for android
+  if (Platform.OS === 'android' && Platform.Version < 23) {
+    return true;
+  }
+
+  const hasPermission = await PermissionsAndroid.check(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  );
+
+  if (hasPermission) {
+    return true;
+  }
+
+  const status = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  );
+
+  if (status === PermissionsAndroid.RESULTS.GRANTED) {
+    return true;
+  }
+
+  if (status === PermissionsAndroid.RESULTS.DENIED) {
+    ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
+  } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+    ToastAndroid.show(
+      'Location permission revoked by user.',
+      ToastAndroid.LONG,
+    );
+  }
+
+  return false;
 };
 
 const Section: React.FC<{
